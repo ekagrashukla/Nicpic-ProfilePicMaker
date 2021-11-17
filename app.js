1f2735ee00651fff42663274e73c9da6e69bc0f4
Ekagra Shukla <ekagrashukla2@gmail.com>
2021-11-17 19:46:57 +0530

    Image Circular Crop Feature Added

--- app.js
+++ app.js
@@ -16,6 +16,34 @@ const bgs = ['./backgrounds/bg000.jpg','./backgrounds/bg001.jpg','./backgrounds/
     './backgrounds/bg004.jpg','./backgrounds/bg005.jpg','./backgrounds/bg006.jpg','./backgrounds/bg007.png',
     './backgrounds/bg008.jpeg']
 
+    const create_circular_image = async (pth) => {
+      var p1 = await Jimp.read(pth);
+      var p2 = await Jimp.read("circle.png");
+    
+      Promise.all([p1, p2]).then((images) => {
+      var lenna = images[0];
+      var mask = images[1];
+      lenna.cover(512,512)
+      // lenna.crop(0,0,512, 512);
+      lenna.mask(mask, 0, 0).write('./output/output_circular/circle_color_result' + counter_circular+ '.png');
+      counter_circular+=1
+    });
+  }
+
+  async function add_background_image(bg) {
+    const image1 = await Jimp.read(bg);
+    const image2 = await Jimp.read('./output/No_bg/no-bg.png');
+    
+    image1.cover(512,512)
+    image2.cover(512,512)
+    image1.blit(image2, 0, 0)
+    
+    // write image
+    .write('./output/temp/'+counter+'.png');
+    create_circular_image('./output/temp/'+counter+'.png');
+    counter+=1
+    console.log('Image Background Added Successfully');
+  }
 
 request.post({
     url: 'https://api.remove.bg/v1.0/removebg',
@@ -34,19 +62,7 @@ request.post({
   });
 
   
-  async function add_background_image(bg) {
-    const image1 = await Jimp.read(bg);
-    const image2 = await Jimp.read('./output/No_bg/no-bg.png');
-    
-    image1.cover(512,512)
-    image2.cover(512,512)
-    image1.blit(image2, 0, 0)
-    
-    // write image
-    .write('./output/temp/'+counter+'.png');
-    counter+=1
-    console.log('Image Background Added Successfully');
-  }
+
 
   bgs.forEach(element => {
     add_background_image(element)
